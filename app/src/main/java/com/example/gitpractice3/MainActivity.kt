532a49bec929174011bd15e79d256c68bc7c6523
Roline Aguilar <69499178+androidRollin@users.noreply.github.com>
2020-09-26 21:51:43 +0800

    final merge change 1

--- app/src/main/java/com/example/gitpractice3/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice3/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("This is contained in my second commit")
         println("Local changes")
-        println("CHANGE 2")
+        println("CHANGE 3")
         println("This line will be merged")
     }
 }
